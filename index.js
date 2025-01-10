d4224f7fdb68b70d1c145b949c600ccd189c7074
plox3 <142830206+plox3@users.noreply.github.com>
2025-01-10 13:48:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to LaewriMarket", "🎮 Playing Cheap Products"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["La Oscura", "La Oscura"];
+const statusTypes = [ 'streaming', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
